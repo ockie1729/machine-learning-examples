61d7707415b5ad03d17da6102676a496ca57189b
OKIMOTO Yusuke <you_oki1@yahoo.co.jp>
2025-02-27 00:50:31 +0900

    Revert "Contrastive Lossを実装"
    This reverts commit 62eb5889ecc7e43c20e20e1deae8fc1322924066.

--- two_tower_model/models.py
+++ two_tower_model/models.py
@@ -5,16 +5,7 @@ from transformers import AutoTokenizer, AutoModel
 
 class TwoTowerModel:
     def __init__(self):
-        self.query_model = Encoder()
-        self.doc_model = Encoder()
-        self.similarity = nn.CosineSimilarity(dim=1, eps=1e-08)
-
-    def forward(self, query: str, doc: str):
-        h_query = self.query_model(query)
-        h_doc = self.doc_model(doc)
-        similarity = self.similarity(h_query, h_doc)
-
-        return similarity
+        pass
 
 
 class Encoder(nn.Module):
@@ -30,21 +21,3 @@ class Encoder(nn.Module):
         output = self.model(**self.tokenizer(input_str, return_tensors="pt"))
 
         return torch.mean(output.last_hidden_state, dim=1, keepdim=False)
-
-
-class ContrastiveLoss(nn.Module):
-    def __init__(self, margin=1.0):
-        super(ContrastiveLoss, self).__init__()
-        self.margin = margin
-
-    def forward(self, y1, y2, t):
-        difference = y1 - y2
-        distance_squared = torch.sum(torch.pow(difference, 2), 1)
-        distance = torch.sqrt(distance_squared)
-
-        negative_distance = self.margin - distance
-        negative_distance = torch.clamp(negative_distance, min=0.0)
-
-        loss = (t * distance_squared + (1 - t) * torch.pow(negative_distance, 2)) / 2.0
-        loss = torch.sum(loss) / y1.size()[0]
-        return loss
