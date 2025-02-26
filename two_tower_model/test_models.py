62eb5889ecc7e43c20e20e1deae8fc1322924066
OKIMOTO Yusuke <you_oki1@yahoo.co.jp>
2025-02-27 00:47:49 +0900

    Contrastive Lossを実装

--- two_tower_model/test_models.py
+++ two_tower_model/test_models.py
@@ -1,21 +1,43 @@
 import unittest
-from models import TwoTowerModel, Encoder
+from models import TwoTowerModel, Encoder, ContrastiveLoss
+import torch
 
 
 class TestTwoTowerModel(unittest.TestCase):
 
+    @unittest.skip("重いので一旦スキップ")
     def test_constructor(self):
         model = TwoTowerModel()
 
+    @unittest.skip("重いので一旦スキップ")
+    def test_forward(self):
+        model = TwoTowerModel()
+
+        similarity = model.forward(query="ラーメン", doc="一風堂 早稲田店")
+
+        print(similarity)
+
 
 class TestEncoder(unittest.TestCase):
-
+    @unittest.skip("重いので一旦スキップ")
     def test_forward(self):
         query_encoder = Encoder()
         output = query_encoder("input")
 
         self.assertEqual(len(output.shape), 2)
 
+
+class TestContrastiveLoss(unittest.TestCase):
+    def test_forward(self):
+        contrastive_loss = ContrastiveLoss()
+
+        y1 = torch.tensor([[0, 0], [0, 0], [0, 0]])
+        y2 = torch.tensor([[2, 0], [0, 0], [1, 0]])
+        t = torch.tensor([[1, 0, 1]])
+
+        loss = contrastive_loss(y1, y2, t)
+
+        self.assert(loss, 1)
 
 if __name__ == "__main__":
     unittest.main()
